74f32523f532c234acbd8c99b5af7c39c6551a2f
Luiz Tanure <letanure@gmail.com>
2026-05-22 10:33:35 +0200

    feat(css): per-component CSS output + wrappers auto-import their styles

--- apps/preview/src/main.tsx
+++ apps/preview/src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@teseor/css";
+import "@teseor/css/reset.css";
+import "@teseor/css/tokens.css";
+import "@teseor/css/base.css";
+import "@teseor/css/utilities.css";
 import "./preview.css";
 import { App } from "./App.tsx";
 
